4f237c04b9000cfe74ec973d08ac5dbcfcb6bb58
✨Bruna ✨ <163605337+Bru67@users.noreply.github.com>
2025-04-19 18:59:42 -0300

    Trabalhando Update Perfil

--- static/perfil.js
+++ static/perfil.js
@@ -8,7 +8,91 @@ document.addEventListener("DOMContentLoaded", () => {
             item.textContent = `${chave}: ${usuario[chave]}`;
             lista.appendChild(item);
         }
-    } else {
-        lista.innerHTML = "<li>Nenhum dado encontrado.</li>";
     }
 });
+
+function adicionarContatos() {
+    const contato = document.getElementById("contatos")
+    const tipo = document.getElementById("")
+
+
+
+    contato.innerHTML = `<li class="list-group-item">
+                            <div class="list-icon">
+                                <i class="fa fa-envelope"></i>
+                            </div>
+                            <div class="list-details">
+                                <span>${contato}</span>
+                                <small>${tipo}</small>
+                            </div>
+                        </li>`
+}
+
+function mostrarOpcoesHabilidades() {
+    document.getElementById('showOpcoesHabilidades').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function fecharOpcoes() {
+    document.getElementById('showOpcoesHabilidades').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
+function validar() {
+    if (checkbox.checked) {
+        window.location.href = "/homepage";
+    } else {
+        mostrarAlerta();
+    }
+
+
+}
+
+
+// Habilidades -> Perfil
+async function carregarHabilidades() {
+    const resposta = await fetch('./habilidades.json');
+    const habilidades = await resposta.json();
+    renderizarCheckboxes(habilidades);
+  }
+
+  function renderizarCheckboxes(habilidades) {
+    const container = document.getElementById("checkboxContainer");
+    container.innerHTML = "";
+    habilidades.forEach(habilidade => {
+      const label = document.createElement("label");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.value = habilidade;
+      label.appendChild(checkbox);
+      label.appendChild(document.createTextNode(" " + habilidade));
+      container.appendChild(label);
+    });
+  }
+
+  async function adicionarHabilidade() {
+    const input = document.getElementById("novaHabilidade");
+    const nova = input.value.trim();
+
+    if (nova === "") {
+      alert("Digite uma habilidade!");
+      return;
+    }
+
+    const resposta = await fetch('/adicionar', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ novaHabilidade: nova })
+    });
+
+    const resultado = await resposta.json();
+
+    if (resposta.ok) {
+      input.value = "";
+      carregarHabilidades();
+    } else {
+      alert(resultado.erro);
+    }
+  }
+
+  carregarHabilidades();
