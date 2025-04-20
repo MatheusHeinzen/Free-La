d9e525c709d7985af2dbc25d0f8f44b8c1cc8c11
Matheus Heinzen <matheusheinzen7@gmail.com>
2025-04-20 20:02:30 -0300

    Navbar superior na tela de atualizar dados

--- static/dados.js
+++ static/dados.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.querySelector(".menu_lateral");
+    const toggleButton = document.getElementById("toggleSidebar");
+
+    toggleButton.addEventListener("click", function(event) {
+        sidebar.classList.toggle("ativo");
+        event.stopPropagation(); 
+    });
+
+    document.addEventListener("click", function(event) {
+        if (!sidebar.contains(event.target) && !toggleButton.contains(event.target)) {
+            sidebar.classList.remove("ativo");
+        }
+    });
     console.log('[DADOS] Página carregada - Iniciando script');
     
     // Configura máscaras
